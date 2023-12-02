8bcc58b0041a0061ea59d0eb4d7b0f11d1d52393
Wren <grahamgrasley@gmail.com>
2023-12-01 23:01:14 -0500

    dynamic version

--- Integration/AlternativeTextures.cs
+++ Integration/AlternativeTextures.cs
@@ -55,7 +55,7 @@ namespace HappyHomeDesigner.Integration
 				ModEntry.monitor.Log("Failed to bind HasVariant", LogLevel.Warn);
 				return;
 			}
-			if (!AltVariantsOf(manager))
+			if (!BindVariantsOf(manager))
 			{
 				ModEntry.monitor.Log("Failed to bind VariantOf", LogLevel.Warn);
 				return;
@@ -93,13 +93,12 @@ namespace HappyHomeDesigner.Integration
 			return true;
 		}
 
-		internal static bool AltVariantsOf(FieldInfo manager)
+		internal static bool BindVariantsOf(FieldInfo manager)
 		{
 			var mg = manager.FieldType.GetMethod("GetAvailableTextureModels");
 			if (!mg.ReturnType.TryGetGenericOf(0, out var modelType))
 				return false;
-			var seasonGetter = modelType.GetProperty("Season", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
-				.GetMethod.CreateDelegate(typeof(Func<,>).MakeGenericType(modelType, typeof(string))) as Func<object, string>;
+			var seasonGetter = modelType.GetProperty("Season", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public).GetMethod;
 
 			VariantsOf = (source, season, list) => {
 				var tm = manager.GetValue(null);
@@ -146,164 +145,13 @@ namespace HappyHomeDesigner.Integration
 			return true;
 		}
 
-		private static void GetVariant(int variant, dynamic model, Furniture furn, Func<object, string> SeasonGetter)
+		private static void GetVariant(int variant, dynamic model, Furniture furn, MethodBase SeasonGetter)
 		{
 			furn.modData[KEY_OWNER] = model.Owner;
 			furn.modData[KEY_NAME] = model.GetId();
 			furn.modData[KEY_VARIATION] = variant.ToString();
-			furn.modData[KEY_SEASON] = SeasonGetter(model);
+			furn.modData[KEY_SEASON] = SeasonGetter.Invoke(model, null);
 			furn.modData[KEY_DISPLAY_NAME] = string.Empty;
-		}
-
-		private static bool BindVariantsOf(FieldInfo manager)
-		{
-			/*
-			* void VariantsOf(Furniture source, string season, List<Furniture> list) {
-			*	List<TextureModel> models = AlternativeTextures.textureManager.GetAvailableTextureModels("Furniture_" + source.Name, season);
-			*	for (int i = 0; i < models.Count; i++) {
-			*		TextureModel m = models[i];
-			*		List<Variation> manVariants = m.ManualVariations;
-			*		for (int j = 0; j < manVariants.Count; j++) {
-			*			if (manVariants[j].Id == -1) {
-			*				int variants = m.Variations;
-			*				<loop>
-			*				return;
-			*			}
-			*		}
-			*		<loop>
-			*	}
-			* }
-			* 
-			* loop:
-			*		for (int j = 0; j < variants.Count; j++) {
-			*			var new_furn = source.getOne();
-			*			new_furn.modData[KEY_OWNER] = m.Owner;
-			*			new_furn.modData[KEY_NAME] = m.GetId();
-			*			new_furn.modData[KEY_VARIATION] = variants[i].ToString();
-			*			new_furn.modData[KEY_SEASON] = m.Season;
-			*			new_furn.modData[KEY_DISPLAY_NAME] = string.Empty;
-			*			list.Add(new_furn);
-			*		}
-			*/
-
-			var getter = manager.FieldType.GetMethod("GetAvailableTextureModels");
-			if (getter is null)
-				return false;
-			if (!getter.ReturnType.TryGetGenericOf(0, out var texModel))
-				return false;
-
-			var variantProp = texModel.GetProperty("ManualVariations");
-			if (variantProp is null)
-				return false;
-			if (!variantProp.PropertyType.TryGetGenericOf(0, out var variantModel))
-				return false;
-
-			var source = Expression.Parameter(typeof(Furniture));
-			var season = Expression.Parameter(typeof(string));
-			var list = Expression.Parameter(typeof(List<Furniture>));
-
-			var i = Expression.Variable(typeof(int));
-			var j = Expression.Variable(typeof(int));
-			var m = Expression.Variable(texModel);
-			var models = Expression.Variable(getter.ReturnType);
-			var variants = Expression.Variable(variantProp.PropertyType);
-			var variantCount = Expression.Variable(typeof(int));
-
-			var breakOuter = Expression.Label("outerLoop");
-			var breakInner = Expression.Label("innerLoop");
-
-			var furn = Expression.Variable(typeof(Furniture));
-			var furnData = Expression.Field(furn, typeof(Furniture).GetField(nameof(Furniture.modData)));
-			var modData = Expression.Variable(typeof(ModDataDictionary));
-
-			var body = Expression.Block(
-				new[] { models, i },
-				Expression.Assign(models, Expression.Call(Expression.Field(null, manager), getter, 
-					Expression.Call(typeof(string).GetMethod(nameof(string.Concat), new[] {typeof(string), typeof(string)}),
-						Expression.Constant("Furniture_"), Expression.Property(source, typeof(Furniture).GetProperty(nameof(Furniture.Name)))
-					),
-					season
-				)),
-				ForInLoop(i, Expression.LessThan(i, Expression.Property(models, getter.ReturnType.GetProperty("Count"))), 
-					Expression.Block(
-						new[] { j, variants, m, variantCount, modData, furn },
-						Expression.Assign(m, Expression.Property(models, getter.ReturnType.GetProperty("Item"), i)),
-						Expression.Assign(variants, Expression.Property(m, variantProp)),
-						Expression.Assign(variantCount, Expression.Property(variants, variantProp.PropertyType.GetProperty("Count"))),
-						ForInLoop(j, Expression.LessThan(j, variantCount), 
-							Expression.IfThen(
-								Expression.Equal(
-									Expression.Property(
-										Expression.Property(variants, variantProp.PropertyType.GetProperty("Item"), j),
-										variantModel.GetProperty("Id")
-									),
-									Expression.Constant(-1)
-								),
-								Expression.Block(
-									Expression.Assign(variantCount, Expression.Property(m, texModel.GetProperty("Variations"))),
-									ForInLoop(j, Expression.LessThan(j, variantCount), 
-										Expression.Block(
-											Expression.Assign(furn, 
-												Expression.Convert(Expression.Call(source, typeof(Furniture).GetMethod(nameof(Furniture.getOne))), typeof(Furniture))
-											),
-											Expression.Assign(modData, Expression.Field(furn, typeof(Furniture).GetField(nameof(Furniture.modData)))),
-											AssignVariant(j, m, modData, texModel),
-											Expression.Call(list, typeof(List<Furniture>).GetMethod(nameof(List<Furniture>.Add)), furn)
-										)
-									),
-									Expression.Goto(breakOuter)
-								)
-							)
-						),
-						ForInLoop(j, Expression.LessThan(j, variantCount),
-							Expression.Block(
-								Expression.Assign(furn, 
-									Expression.Convert(Expression.Call(source, typeof(Furniture).GetMethod(nameof(Furniture.getOne))), typeof(Furniture))
-								),
-								Expression.Assign(modData, Expression.Field(furn, typeof(Furniture).GetField(nameof(Furniture.modData)))),
-								AssignVariant(
-									Expression.Property(Expression.Property(variants, variantProp.PropertyType.GetProperty("Item"), j), variantModel.GetProperty("Id")), 
-									m, modData, texModel),
-								Expression.Call(list, typeof(List<Furniture>).GetMethod(nameof(List<Furniture>.Add)), furn)
-							)
-						)
-					)
-				),
-				Expression.Label(breakOuter)
-			);
-
-			VariantsOf = Expression.Lambda<Action<Furniture, string, List<Furniture>>>(body, source, season, list).Compile();
-
-			return true;
-		}
-
-		private static Expression AssignVariant(Expression variant, Expression model, Expression data, Type modelType)
-		{
-			var dataIndex = typeof(ModDataDictionary).GetProperty("Item");
-			return Expression.Block(
-				Expression.Assign(Expression.Property(data, dataIndex, Expression.Constant(KEY_OWNER)), 
-				Expression.Property(model, modelType.GetProperty("Owner"))),
-				Expression.Assign(Expression.Property(data, dataIndex, Expression.Constant(KEY_NAME)), 
-				Expression.Call(model, modelType.GetMethod("GetId"))),
-				Expression.Assign(Expression.Property(data, dataIndex, Expression.Constant(KEY_VARIATION)), 
-				Expression.Call(variant, typeof(int).GetMethod(nameof(int.ToString), Array.Empty<Type>()))),
-				Expression.Assign(Expression.Property(data, dataIndex, Expression.Constant(KEY_SEASON)), 
-				Expression.Property(model, modelType.GetProperty("Season", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))),
-				Expression.Assign(Expression.Property(data, dataIndex, Expression.Constant(KEY_DISPLAY_NAME)), 
-				Expression.Constant(string.Empty))
-			);
-		}
-
-		private static Expression ForInLoop(Expression i, Expression condition, Expression Block)
-		{
-			var exit = Expression.Label();
-			return Expression.Block(
-				Expression.Assign(i, Expression.Constant(0)),
-				Expression.Loop(
-					Expression.IfThenElse(condition, Expression.Block(Block, Expression.Increment(i)), Expression.Break(exit))
-				),
-				Expression.Label(exit)
-			);
 		}
 	}
 }
